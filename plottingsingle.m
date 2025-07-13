dbcf916f9f5b7fb3850a6b0a1ddd24af90e557be
Mohammad Asad Akhtar <141022885+md-asad-akhtar@users.noreply.github.com>
2025-07-13 14:48:52 +0200

    For single Plotted Figure Graph

---
+++ plottingsingle.m
@@ -0,0 +1,17 @@
+clc;
+clear;
+
+
+filename = fullfile('datasets','mnist_test_10.csv');
+data = readmatrix(filename);
+index = 5;  
+
+% specify digit
+label = data(index, 1);
+pixels = data(index, 2:end);
+
+
+image = reshape(pixels, [28, 28])';
+figure;
+imshow(image, []);
+title(['Label: ', num2str(label)]);
